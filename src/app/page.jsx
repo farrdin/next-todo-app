d135960c423100d6b38dfacd224e5f5de15aab15
Fardin Ahmed Alif <Alif.Ahmed44@gmail.com>
2024-10-28 01:50:24 +0600

    Project Setup

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Header />
+      <h1> Hello</h1>
+      <Footer />
+    </div>
+  );
+}
